b461dd4275dee04488281fafadf683e29dfa0c01
Glen E. Ivey <gleneivey@wontology.org>
2009-03-28 23:19:39 -0700

    Corrected bug causing Pickler to treat "started" the same as "unstarted" (supressing pulls) when filtering stories.  Added test case to confirm correct filter operation.

--- spec/pull_all_spec.rb
+++ spec/pull_all_spec.rb
@@ -1,14 +1,17 @@
 require File.join(File.dirname(__FILE__),'spec_helper')
 
 describe Pickler, "when pulling all features from Tracker" do
-  before do
+  before(:all) do
     @domain     = "http://www.pivotaltracker.com:80"
     @service    = "/services/v2/"
+
+    @featuresPath = File.join( File.dirname(__FILE__), '..', 'features', '' )
+    # test data in "*.feature.original" files
+    FileUtils.rm Dir.glob( @featuresPath + '**/*.feature')
   end
 
   after do # clean up the files the test creates
-    FileUtils.rm Dir.glob( File.join( File.dirname(__FILE__), '..', 'features'
-      ) + '/**/*.feature') # test data in "*.feature.original" files
+    FileUtils.rm Dir.glob( @featuresPath + '**/*.feature')
   end
 
 
@@ -22,7 +25,7 @@ describe Pickler, "when pulling all features from Tracker" do
 
   it "should GET the story list with a filter" do
 
-    # make a mock to check the URL for getting the story list
+    # make a mock to check the URLs of requests Pickler generates
     mockHttp = mock("Mock-HTTP")
 
     # for all requests, get responses from FakeWeb; check each path for filter
@@ -46,18 +49,20 @@ describe Pickler, "when pulling all features from Tracker" do
   end
 
 
-        # story states: 2 unscheduled, 3 unstarted, 5 started, 4 finished,
-        #               6 delivered, 1 accepted, 7 rejected
-  it "should not GET stories in states: unscheduled, unstarted" do
-
-
-  end
-
-
+        # test data, story [number] [state]
+        #   2 unscheduled, 3 unstarted, 5 started, 4 finished,
+        #   6 delivered, 1 accepted, 7 rejected
   it "should write .feature files for pulled stories" do
     begin
       Pickler.run([ "pull" ])
     rescue SystemExit
+    end
+
+    File.exist?( @featuresPath + "2.feature" ).should be_false
+    File.exist?( @featuresPath + "3.feature" ).should be_false
+    [ 1, 4, 5, 6, 7 ].each do |feat|
+      File.readlines( @featuresPath + feat.to_s + ".feature").should eql(
+        File.readlines( @featuresPath + feat.to_s + ".feature.original") )
     end
   end
 end
